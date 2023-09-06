7c7510893809538049bf218937b7d699a0d882c6
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2023-09-06 19:37:31 +0700

    isi manual database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,51 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+
+        User::create([
+            'name' => 'faisal ridhani',
+            'email' => 'thinkbear@gmail.com',
+            'password' => bcrypt('password')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design'
+        ]);
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'category_id' => 2,
+            'excerpt' => "igma reqwas",
+            'body'=> "<p>enim at in obcaecati tempora beatae laudantium voluptate aperiam
+                    excepturi delectus recusandae nemo aliquam deleniti temporibus itaque? Modi,
+                    quod voluptate. Omnis, dolor eveniet nemo aliquam amet iusto.</p>
+                    <p>enim at in obcaecati tempora beatae laudantium voluptate aperiam
+                    excepturi delectus recusandae nemo aliquam deleniti temporibus itaque? Modi,
+                    quod voluptate. Omnis, dolor eveniet nemo aliquam amet iusto.</p>",
+            'user_id' => 1
+        ]);
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-ke-dua',
+            'category_id' => 1,
+            'excerpt' => "igma reqwas",
+            'body'=> "<p>enim at in obcaecati tempora beatae laudantium voluptate aperiam
+                    excepturi delectus recusandae nemo aliquam deleniti temporibus itaque? Modi,
+                    quod voluptate. Omnis, dolor eveniet nemo aliquam amet iusto.</p>
+                    <p>enim at in obcaecati tempora beatae laudantium voluptate aperiam
+                    excepturi delectus recusandae nemo aliquam deleniti temporibus itaque? Modi,
+                    quod voluptate. Omnis, dolor eveniet nemo aliquam amet iusto.</p>",
+            'user_id' => 1
+        ]);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.main')
 @section('container')
     @foreach($posts as $post)
-        <article class="mb-5">
-            <h2><a href="/posts/{{ $post->slug }}">{{$post-> title}}</a></h2>
-            <h5>By: {{$post->author}}</h5>
+        <article class="mb-5 border-bottom pb-3">
+            <h2><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{$post-> title}}</a></h2>
+            <h5>By: faisal in <a href="/categories/{{$post->category->slug}}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
             <p>{{$post->excerpt}}</p>
+            <a href="/posts/{{$post->slug}}" class="text-decoration-none">Read More..</a>
         </article>
+
     @endforeach
 @endsection
